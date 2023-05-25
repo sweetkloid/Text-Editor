b220a5d9c934f62981fbcee714fff7a6b3e77ed3
Sami <coolsweetsami@gmail.com>
2023-05-24 20:55:42 -0400

    updates to database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -14,31 +14,18 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 
-export const putDb = (id, content) => {
-  return new Promise((resolve, reject) => {
-    const request = openDB('jate', 1)
-      .then(jatedb => {
-        const tx = jatedb.transaction('jate', 'readwrite');
-        const store = tx.objectStore('jate');
-        const addRequest = store.add({ id, content });
-        addRequest.onsuccess = () => {
-          tx.complete.then(() => {
-            console.log('Content added to database');
-            resolve();
-          });
-        };
-        addRequest.onerror = (error) => {
-          console.error('Error adding content to database:', error);
-          reject(error);
-        };
-      })
-      .catch(error => {
-        console.error('Error opening database:', error);
-        reject(error);
-      });
-  });
+export const putDb = async (content) => {
+  try {
+    const jatedb = await openDB('jate', 1);
+    const tx = jatedb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request= store.add({content});
+    const result = await request;
+    console.log('Content added to database:', result);
+  } catch (error) {
+    console.error('Error adding content to database:', error);
+  }
 };
-
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
